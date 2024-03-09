1bd871d3da438d98e69547fc650deed7263aafe9
Gary <amezheritskiy@gmail.com>
2024-03-08 19:19:34 -0500

    fixing lang

--- static/common.js
+++ static/common.js
@@ -73,6 +73,7 @@ function toPlace(number, place) {
 let LANG = getCookie('language') === '' ? 'English' : getCookie('language');
 
 function readJSON(stringName) {
+    console.log(stringName.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&#39;/g, '"').replace(/&#34;/g, '"').replace(/~/g, "'").replace(/None/g, 'null').replace(/False/g, false).replace(/True/g, true).replace(/Ã—/g, '×').replace(/ÄŒ/g, 'Č').replace(/Ã¶/g, 'ö').replace(/Ã¤/g, 'ä').replace(/&amp;/g, '&').replace(/Ð¡/g, 'С').replace(/â€“/g, '–').replace(/Â/g, ' '))
     return JSON.parse(stringName.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&#39;/g, '"').replace(/&#34;/g, '"').replace(/~/g, "'").replace(/None/g, 'null').replace(/False/g, false).replace(/True/g, true).replace(/Ã—/g, '×').replace(/ÄŒ/g, 'Č').replace(/Ã¶/g, 'ö').replace(/Ã¤/g, 'ä').replace(/&amp;/g, '&').replace(/Ð¡/g, 'С').replace(/â€“/g, '–').replace(/Â/g, ' '));
 }
 
